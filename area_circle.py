0a87132f8d12c37619646e1bf63dd7b7bd65bb3e
sadik al jarif <78945097+jarif87@users.noreply.github.com>
2022-11-30 17:13:32 +0600

    Add files via upload

---
+++ area_circle.py
@@ -0,0 +1,37 @@
+def area_circle(r):
+    pi=3.14
+    return pi*(r*r)
+
+
+print("Area is of circle is %.2f" % area_circle(5))
+
+
+#%%
+
+def area_circle(r):
+    
+    pi=3.14
+     
+    x=pi*(r*r)
+    return x
+   
+   
+   
+r=float(input("ENter your number :"))
+print("value is %.2f " % area_circle(r))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
